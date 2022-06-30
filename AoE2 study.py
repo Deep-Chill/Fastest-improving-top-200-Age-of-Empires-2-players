447486607796aa146c7cc040086d4fff62cbd40b
Deep-Chill <65825972+Deep-Chill@users.noreply.github.com>
2022-06-30 16:25:46 -0500

    Update AoE2 study.py

--- AoE2 study.py
+++ AoE2 study.py
@@ -55,13 +55,6 @@ for i in df.head(200)['profile_id']:
             else:
                 pass
 
-        # for j in list_of_ratings[:-100]:
-        #     if j.get('rating') == max_rating:
-        #         df.loc[df['profile_id'] == i, 'date_of_max_rating'] = dt.datetime.fromtimestamp(j.get('timestamp'))
-        #     elif j.get('rating') == min_rating:
-        #         df.loc[df['profile_id'] == i, 'date_of_min_rating'] = dt.datetime.fromtimestamp(j.get('timestamp'))
-        #     else:
-        #         pass
         '''get the difference in rating'''
         df.loc[df['profile_id'] == i, 'difference_in_rating'] = max_rating - min_rating
 
@@ -77,95 +70,3 @@ df.drop(['Unnamed: 0'], axis=1, inplace=True)
 df = df[df['max_rating'] != 0]
 
 print(df.sort_values(by='min_rating', ascending=True).head(10))
-
-
-
-    # rating_history = API.get_rating_history(profile_id=i)
-    # print(i, rating_history)
-
-# list_of_ratings = API.get_rating_history(profile_id=df['profile_id'][0])
-# '''get the value of rating from the rating history of the first player'''
-# for i in list_of_ratings:
-#     print(list_of_ratings.get('rating'))
-
-# '''find the lowest rating of all players in df'''
-#
-# a = API.get_leaderboard(search='TheViper', json=True)
-#
-# df = pd.read_json(a)
-#
-# print(df)
-
-# # df = pd.read_json('https://aoe2.net/api/player/ratinghistory?game=aoe2de&leaderboard_id=3&steam_id=76561199003184910&count=5')
-#
-# print(df)
-
-# df = pd.read_csv('AoE2_list_of_top_10000_players.csv')
-# for s in df['profile_id'].head():
-#     ratings_of_player = API.get_rating_history(profile_id=s)
-#     a = []
-#     for i in ratings_of_player:
-#         a.append(i.get('rating'))
-#     print(a)
-#     df['max_rating'] = max(a)
-#
-# print(df.head())
-# ratings_of_player = API.get_rating_history(profile_id='199325')
-# a = []
-# for i in ratings_of_player:
-#     a.append(i.get('rating'))
-# print(max(a))
-
-#
-# profile_iden = df['profile_id']
-# a = []
-# for i in profile_iden.head():
-#     rating_hist = API.get_rating_history(profile_id=str(i))
-#     a.append(rating_hist)
-#
-# print(a)
-
-# for i in df.head():
-#     print(i.loc['name'])
-    # # print(df['profile_id'])
-    # profile_iden = df['profile_id']
-    # rating_hist = API.get_rating_history(profile_id=profile_iden)
-    # print(rating_hist)
-
-# print(df.tail())
-
-# leaderboard = API.get_leaderboard(leaderboard_id=3, count=10000)
-#
-# a = leaderboard.get('leaderboard')
-# df = pd.DataFrame()
-# for i in a:
-#     df = df.append(i, ignore_index=True)
-#
-# df.to_csv('AoE2_list_of_top_10000_players.csv')
-
-
-# json_object = json.dumps(leaderboard, indent=4)
-#
-# df = pd.read_json(json_object)
-
-# print(leaderboard)
-
-# rating_hist = API.get_rating_history(profile_id=196240)
-
-# rating = []
-# date = []
-#
-# df = pd.DataFrame()
-# df['Rating'] = None
-#
-# for i in rating_hist:
-#     rating.append(i['rating'])
-#     date.append(dt.datetime.fromtimestamp(i['timestamp']))
-#
-# df['Rating'] = rating
-# df['Timestamp'] = date
-# print(df[df['Rating'] == df['Rating'].min()])
-#
-# # '''create a chart showing the rating history of the player'''
-# # plt.plot(df['Timestamp'], df['Rating'])
-# # plt.show()
